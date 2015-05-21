1b6f677ac1c080749291e07cc4b6d2ede8845833
Cyncro <matthewfrassetti@gmail.com>
2015-05-21 09:24:23 -0400

    Sample introduction

---
+++ metroid_game.py
@@ -0,0 +1,36 @@
+#The first line ever read...
+print('"The last Metroid is in captivity. The galaxy is at peace..."')
+
+#Function to run the intro of the game.
+def Intro():
+    print('Congratulations! You are now the owner of a new baby Metroid!')
+    print('Please fill out the following information to keep on record with the Metroid Ownership Agency.')
+    print('Your Name:')
+    global PlayerName
+    PlayerName = input()
+    print('Your Age:')
+    global PlayerAge
+    PlayerAge = input()
+    print('What will you name your new Metroid?')
+    global MetroidName
+    MetroidName = input()
+    Confirm()
+
+#Function to run the confirmation sequeunce/loop.
+def Confirm():
+    print('Are you sure? Y/N')
+    Answer = input()
+    if Answer is ('Y'):
+        print ('Your name: ' + PlayerName)
+        print ('Your age: ' + PlayerAge)
+        print ('Metroid name: ' + MetroidName)
+    elif Answer is ('N'):   
+        Intro()
+    else:
+        print('I did not understand. Could you repeat that?')
+        Confirm()
+        
+#The Game
+Intro()
+    
+    
